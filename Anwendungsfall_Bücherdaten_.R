7313513edb5bd58c2f8748adc31f33f0430d1342
A-Xander <58584903+A-Xander@users.noreply.github.com>
2020-06-28 21:31:40 +0200

    Update

---
+++ Anwendungsfall_Bücherdaten_.R
@@ -0,0 +1,35 @@
+# Laden der sparklyr Bibliothek in die R Session
+library(sparklyr)
+library(caTools)
+
+# Lokale Spark Verbindung herstellen
+sc <- spark_connect(master = "local")
+
+# Datensatz als R Dataframe abspeichern
+book_ratings_df <- read.delim("Buecherdaten/book_ratings.dat") # Bewertungsmatrix
+items_info_df <- read.csv("Buecherdaten/items_info.csv", TRUE,";") # Informationen
+names(items_info_df)[1] <- "item"
+users_info_df <- read.csv("Buecherdaten/bookdata/users_info.csv", TRUE,";") # Informationen
+names(users_info_df)[1] <- "user"
+
+# Seed setzen, damit die gleiche Aufteilung wieder verwendet werden kann
+set.seed(101) 
+
+# Aufteilen in Training und Test Daten
+sample = sample.split(book_ratings_df$user, SplitRatio = .75)
+train_df = subset(book_ratings_df, sample == TRUE)
+test_df  = subset(book_ratings_df, sample == FALSE)
+
+# Datensatz als Spark Tabelle in R wrappen
+train_tbl <- sdf_copy_to(sc, train_df)
+test_tbl <- sdf_copy_to(sc, test_df)
+
+# Ausfuehren des ALS Algorithmus zum Trainieren eines Modelles
+model <- ml_als(train_tbl, rating ~ user + item)
+
+# Vorhersage anhand des Modelles
+predictions_df <- data.frame(ml_predict(model, test_tbl))
+
+# Einf�gen der Informationen aus dem Nutzer und B�cher Datensatz
+predictions_info_df <- merge(predictions_df, users_info_df,  by.predictions_df = "user", by.users_info_df = 'user')
+predictions_info_df <- merge(predictions_info_df, items_info_df,  predictions_info_df = "item", items_info_df = 'item')
