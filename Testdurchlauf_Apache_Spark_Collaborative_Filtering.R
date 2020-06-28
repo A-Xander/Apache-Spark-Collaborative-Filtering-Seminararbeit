44a6746251ecb3d647dec0c8c6a137606be99e3a
A-Xander <58584903+A-Xander@users.noreply.github.com>
2020-06-28 12:10:17 +0200

    Update

---
+++ Testdurchlauf_Apache_Spark_Collaborative_Filtering.R
@@ -0,0 +1,22 @@
+# Laden der sparklyr Bibliothek in die R Session
+library(sparklyr)
+
+# Lokale Spark Verbindung herstellen
+sc <- spark_connect(master = "local")
+
+# Datensatz als R Dataframe abspeichern
+example_df <- data.frame(
+  user   = c(1, 2, 0, 1, 2, 0),
+  item   = c(1, 1, 1, 2, 2, 0),
+  rating = c(3, 1, 2, 4, 5, 4)
+)
+
+# Datensatz als Spark Tabelle in R wrappen
+train_tbl <- sdf_copy_to(sc, example_df)
+test_tbl <- sdf_copy_to(sc, example_df)
+
+# Ausführen des ALS Algorithmus zum Trainieren eines Modelles
+model <- ml_als(train_tbl, rating ~ user + item)
+
+# Vorhersage anhand des Modelles
+ml_predict(model, test_tbl)
